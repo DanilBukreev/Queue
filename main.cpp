2afd930dc0db3a9050750e1710d36ce70e35a8f7
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2018-04-21 23:46:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
-#include <queue.hpp>
-#include <iostream>
+#include "Queue.hpp"
 
 int main() {
-  Queue q1;
-
-  Queue q2 = {1, 2, 3, 4};
-  Queue q3{q2};
-  cout << q2 << endl;
-  cout << q3 << endl;
-  q1.swap(q3);
-  cout << q1 << endl;
-  cout << boolalpha << q3.empty();
-  cout << q1.size() << endl;
-  q3.push(5);
-  cout << q3 << endl;
-  ofstream output{"queue.bin"};
-  output << q3;
-  Queue q4;
-  ifstream input{"queue.bin"};
-  input >> q4;
-
-  cout << (q4 == q3) << endl;
-
-  cout << q4.front() << endl;
-  cout << q5.back() << endl;
-
-  q4.pop();
-  cout << q4 << endl;
-  q4.front() = 6;
+    Queue <int> q1;
+    Queue <int> q2 = {1, 2, 3, 4};
+    Queue <int> q3 {q2};
+    std::cout << q2 << std::endl;
+    std::cout << q3 << std::endl;
+    q1.swap(q3);
+    std::cout << q1 << std::endl;
+    std::cout << boolalpha
+    << q3.empty()<< endl;
+    std::cout << q1.size() << std::endl;
+    q3.push(5);
+    std::cout << q3 << std::endl;
+    std::ofstream output { "queue.bin" };
+    output << q3;
+    output.close();
+    Queue <int> q4;
+    std::ifstream input { "queue.bin" };
+    input >> q4;
+    input.close();
+    std::cout << (q4 == q3) << std::endl;
+    std::cout << q4.front() << std::endl;
+    std::cout << q4.back() << std::endl;
+    q4.pop();
+    std::cout << q4 << std::endl;
+    q4.front() = 6;
 }
